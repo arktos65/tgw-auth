be304fbb3452d412054e9582ad48b26a4d940eb3
arktos65 <sean@tgwconsulting.co>
2019-03-26 08:14:48 -0600

    Installed and configured rswag gem.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,6 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  # API documentation
+  mount Rswag::Ui::Engine => '/api-docs'
+  mount Rswag::Api::Engine => '/api-docs'
+
 end
