b6b6ef5dbc5d562e1d03fec7cd8325bbf7ad0649
Sean Sullivan <sean@tgwconsulting.co>
2019-03-28 04:19:53 -0600

    Added basic Devise authentication setup.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  root to: "home#index"
+  # get 'home/index'
+
   # API documentation
   mount Rswag::Ui::Engine => '/api-docs'
   mount Rswag::Api::Engine => '/api-docs'
