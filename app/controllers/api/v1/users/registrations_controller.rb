f91534370936851129674a92a98aa162b253d64c
Sean Sullivan <sean@tgwconsulting.co>
2019-03-29 06:20:43 -0600

    Integrate Devise and Doorkeeper.

--- app/controllers/api/v1/users/registrations_controller.rb
+++ app/controllers/api/v1/users/registrations_controller.rb
@@ -1,8 +1,41 @@
 # frozen_string_literal: true
-
+#
+# app/controllers/application_controller.rb
+#
+# Copyright 2019 TGW Consulting, LLC. All rights reserved.
+#
+# This source code is proprietary, confidential information of TGW Consulting, LLC.
+# It contains TGW Consulting intellectual property, including trade secrets and
+# copyright-protected authorship, and may include patentable inventions. You may
+# not distribute this source code outside of TGW Consulting without express written
+# permission from management. TGW Consulting does not claim ownership of included
+# open source software components, which are subject to their own licenses.
+#
 class Api::V1::Users::RegistrationsController < Devise::RegistrationsController
   # before_action :configure_sign_up_params, only: [:create]
   # before_action :configure_account_update_params, only: [:update]
+
+  # POST /resource
+  def create
+    build_resource(sign_up_params)
+    resource.save
+    if resource.persisted?
+      if resource.active_for_authentication?
+        # set_flash_message! :notice, :signed_up
+        # To avoid login comment out sign_up method
+        # sign_up(resource_name, resource)
+        render json: resource # , location: after_sign_up_path_for(resource)
+      else
+        # set_flash_message! :notice, :"signed_up_but_#{resource.inactive_message}"
+        expire_data_after_sign_in!
+        render json: resource # , location: after_inactive_sign_up_path_for(resource)
+      end
+    else
+      clean_up_passwords resource
+      set_minimum_password_length
+      respond_with resource
+    end
+  end
 
   # GET /resource/sign_up
   # def new
