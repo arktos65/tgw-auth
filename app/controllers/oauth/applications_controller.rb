4e877f379b2c76ebee0c1d171a5663c226d9c63c
arktos65 <sean@tgwconsulting.co>
2020-02-21 08:20:29 -0700

    Added Doorkeeper views for customization.

--- app/controllers/oauth/applications_controller.rb
+++ app/controllers/oauth/applications_controller.rb
@@ -2,16 +2,24 @@
 
 # app/controllers/oauth/applications_controller.rb
 class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
-  before_action :authenticate_user! # use before_action instead if on Rails 5.1+
+  layout "doorkeeper/admin" unless Doorkeeper.configuration.api_only
+
+  before_action :authenticate_admin!
+  before_action :set_application, only: %i[show edit update destroy]
 
   def index
-    @applications = current_user.oauth_applications
+    @applications = Application.ordered_by(:created_at)
+
+    respond_to do |format|
+      format.html
+      format.json { head :no_content }
+    end
   end
 
   # only needed if each application must have some owner
   def create
     @application = Doorkeeper::Application.new(application_params)
-    @application.owner = current_user if Doorkeeper.configuration.confirm_application_owner?
+    @application.owner = current_admin if Doorkeeper.configuration.confirm_application_owner?
     if @application.save
       flash[:notice] = I18n.t(:notice, :scope => [:doorkeeper, :flash, :applications, :create])
       redirect_to oauth_application_url(@application)
@@ -22,6 +30,6 @@ class Oauth::ApplicationsController < Doorkeeper::ApplicationsController
 
   private
     def set_application
-      @application = current_user.oauth_applications.find(params[:id])
+      @application = current_admin.oauth_applications.find(params[:id])
     end
 end
